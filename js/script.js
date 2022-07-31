7e94e2af2db2811704add1675e9d6071a2100498
Michał Chmurski <chmurski.michal@gmail.com>
2022-07-31 20:34:19 +0200

    added new actions in javascript

--- js/script.js
+++ js/script.js
@@ -1,28 +1,42 @@
 'use strict';
 
 function titleClickHandler(event){
-    console.log(event);
-  
-    /* remove class 'active' from all article links  */
+    event.preventDefault();
+    const clickedElement = this;
+    console.log('Link was clicked');
+      
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
   
-    /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.posts a.post active');
+    /* [DONE] remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts .active');
 
     for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('post active');
+      activeArticle.classList.remove('active');
     }
   
     /* get 'href' attribute from the clicked link */
-  
+
+    const articleSelector = document.querySelector('href');
+    let href = clickedElement.getAttribute("href");
+    console.log('link was clicked?');
+         
+   
     /* find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(href);
   
     /* add class 'active' to the correct article */
+    
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
