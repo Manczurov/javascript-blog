fbe59c5c59850f99a88d443600171168c85bfb44
Michał Chmurski <chmurski.michal@gmail.com>
2022-08-01 21:01:57 +0200

    clean javascript from no neede console.logs

--- js/script.js
+++ js/script.js
@@ -3,39 +3,26 @@
 function titleClickHandler(event){
     event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked');
       
-    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
   
-    /* [DONE] remove class 'active' from all articles */
-
     const activeArticles = document.querySelectorAll('.posts .active');
 
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
-  
-    /* get 'href' attribute from the clicked link */
-
-    const articleSelector = document.querySelector('href');
-    let href = clickedElement.getAttribute("href");
-    console.log('link was clicked?');
-         
-   
-    /* find the correct article using the selector (value of 'href' attribute) */
-
-    const targetArticle = document.querySelector(href);
-  
-    /* add class 'active' to the correct article */
     
+    let href = clickedElement.getAttribute("href");
+         
+    const targetArticle = document.querySelector(href);
+      
     targetArticle.classList.add('active');
   }
   
